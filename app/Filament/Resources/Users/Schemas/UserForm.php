c7a9e2b7f4ddcbc6445695f291c0e15d30c1fa16
igniafungi <gerencia@igniafungi.com>
2025-12-25 15:18:59 -0500

    recurso de usuarios

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Filament\Resources\Pages\CreateRecord;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email Address')
+                    ->email()
+                    ->maxlength(255)
+                    ->unique(ignoreRecord: true)
+                    ->required(),
+                DateTimePicker::make('email_verified_at'),
+                TextInput::make('password')
+                    ->password()
+                    ->dehydrated(fn($state) => filled($state))
+                    ->required(fn($livewire) => $livewire instanceof CreateRecord),
+            ]);
+    }
+}
