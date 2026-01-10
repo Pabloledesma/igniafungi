42e15f90ecf9b917a0ede9abc9511dcd27dee0b5
igniafungi <gerencia@igniafungi.com>
2026-01-10 15:41:03 -0500

    se agrega la migración del campo stock en productos

---
+++ database/migrations/2026_01_10_203535_add_stock_to_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Solo intentamos crearla si NO existe
+        if (!Schema::hasColumn('products', 'stock')) {
+            Schema::table('products', function (Blueprint $table) {
+                $table->decimal('stock', 10, 3)->default(0)->after('price');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
